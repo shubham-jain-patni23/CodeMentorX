38a5d1e337034860393892f532dc1a0757a28298
Shubham Jain <shubham.jain.patni23@gmail.com>
2026-01-16 16:43:45 +0000

    Enable JSON parsing and add test POST API endpoint

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,23 @@ const express = require("express");
 
 const app = express();
 
+// 🔹 NEW LINE (middleware)
+app.use(express.json());
+
 // simple test route
 app.get("/", (req, res) => {
   res.send("CodeMentorX backend is running 🚀");
 });
+
+app.post("/test", (req, res) => {
+  const data = req.body;
+
+  res.json({
+    message: "Data received successfully",
+    receivedData: data,
+  });
+});
+
 
 const PORT = 5000;
 
